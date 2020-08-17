64df13597838bab2da50dd56e4825b1576238972
Rovern <cutiegirl936@hotmail.com>
2020-08-17 12:57:03 +0800

    Updates

---
+++ precache-manifest.f2fb0f086505c88840070f140e404c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab027c907b787c8879b6e631206b5053",
+    "url": "/xrovTicTacToe/index.html"
+  },
+  {
+    "revision": "dcf0095ac5462a799924",
+    "url": "/xrovTicTacToe/static/css/main.2c6db9df.chunk.css"
+  },
+  {
+    "revision": "5685a142f4197c419ea8",
+    "url": "/xrovTicTacToe/static/js/2.5f34c475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xrovTicTacToe/static/js/2.5f34c475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf0095ac5462a799924",
+    "url": "/xrovTicTacToe/static/js/main.60808255.chunk.js"
+  },
+  {
+    "revision": "0f76e8944d5f0c1c1c62",
+    "url": "/xrovTicTacToe/static/js/runtime-main.db89a8f6.js"
+  }
+]);
